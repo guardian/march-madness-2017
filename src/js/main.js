f15a65e5d8cc825a3dfa50f185dbed882a8d314a
sammorrisdesign <mail@sammorrisdesign.co.uk>
2017-02-23 17:37:36 -0500

    add tabs for mobile and make them selectable and add classes to main element

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,7 @@
 var share = require('./modules/share.js');
 var bracket = require('./modules/bracket.js');
+var tabs = require('./modules/tabs.js');
 
 share.init();
 bracket.init();
+tabs.init();
---
+++ src/js/modules/tabs.js
@@ -0,0 +1,21 @@
+var $ = require('../vendor/jquery.js');
+
+module.exports =  {
+    init: function() {
+        this.bindings();
+    },
+
+    bindings: function() {
+        $('.march-tabs__option').click(function(element) {
+            this.selectTab(element.currentTarget);
+        }.bind(this));
+    },
+
+    selectTab: function(element) {
+        var selectedTab = $(element).attr('data-region');
+        $('.march-tabs .is-selected').removeClass('is-selected');
+        $(element).addClass('is-selected');
+
+        $('.march').removeClass().addClass('march march--' + selectedTab);
+    }
+}
