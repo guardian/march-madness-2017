410b2232d579ccb3c3108901990f648df1245d4e
sammorrisdesign <mail@sammorrisdesign.co.uk>
2017-02-24 15:21:22 -0500

    tabs are sticky

--- src/js/modules/tabs.js
+++ src/js/modules/tabs.js
@@ -1,14 +1,29 @@
 var $ = require('../vendor/jquery.js');
 
+var tabsPosition;
+
 module.exports =  {
     init: function() {
         this.bindings();
+        this.getValues();
     },
 
     bindings: function() {
         $('.march-tabs__option').click(function(element) {
             this.selectTab(element.currentTarget);
         }.bind(this));
+
+        $(window).scroll(function() {
+            this.onScroll();
+        }.bind(this));
+
+        $(window).resize(function() {
+            this.getValues();
+        }.bind(this));
+    },
+
+    getValues: function() {
+        tabsPosition = $('.march-tabs').offset().top;
     },
 
     selectTab: function(element) {
@@ -18,5 +33,14 @@ module.exports =  {
 
         $('.march').removeClass().addClass('march march--' + selectedTab);
         $('.march-scroll').scrollLeft(0);
+    },
+
+    onScroll: function() {
+        if ($(window).scrollTop() > tabsPosition) {
+            $('.march-tabs').addClass('is-sticky');
+        } else {
+            $('.march-tabs').removeClass('is-sticky');
+            console.log('eh?');
+        }
     }
 }
