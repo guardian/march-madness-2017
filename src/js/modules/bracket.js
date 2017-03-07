b28132cb5fcbf4938aaa697e0875ff8227139f52
sammorrisdesign <mail@sammorrisdesign.co.uk>
2017-03-07 18:12:12 -0500

    add support for double teams

--- src/js/modules/bracket.js
+++ src/js/modules/bracket.js
@@ -65,21 +65,25 @@ module.exports =  {
         var html = $(team).html(),
             matchNumber = $(team).parent().data('match-number'),
             isFirst = (matchNumber % 2) == 1,
+            classList = ($(team).hasClass('is-double') ? 'is-double is-filled' : 'is-filled'),
             region = $(team).parent().parent().data('region'),
             round = $(team).parent().parent().data('round'),
             teamName = $(team).attr('data-team');
 
+            console.log(classList);
+            console.log($(team));
+
         if (round === 6) {
-            $('.march-round--winner .march-bracket__team').attr('data-team', teamName).html(html).addClass('is-filled');
+            $('.march-round--winner .march-bracket__team').attr('data-team', teamName).html(html).addClass(classList);
         } else if (round === 5) {
             var isFirst = region === 'east midwest';
-            $('.march-round--final .march-bracket__team:' + (isFirst ? 'first-of-type' : 'last-of-type')).attr('data-team', teamName).html(html).addClass('is-filled');
+            $('.march-round--final .march-bracket__team:' + (isFirst ? 'first-of-type' : 'last-of-type')).attr('data-team', teamName).html(html).addClass(classList);
         } else if (round === 4) {
             var isTop = region === 'east' || region === 'midwest';
             var isFirst = region === 'east' || region === 'south';
-            $('.march-round--semis:' + (isTop ? 'first-of-type' : 'last-of-type') + ' .march-bracket__matchup .march-bracket__team:' + (isFirst ? 'first-of-type' : 'last-of-type')).attr('data-team', teamName).html(html).addClass('is-filled');
+            $('.march-round--semis:' + (isTop ? 'first-of-type' : 'last-of-type') + ' .march-bracket__matchup .march-bracket__team:' + (isFirst ? 'first-of-type' : 'last-of-type')).attr('data-team', teamName).html(html).addClass(classList);
         } else {
-            $('.march-round[data-region="' + region + '"][data-round="' + (round + 1) + '"] .march-bracket__matchup[data-match-number="' + Math.ceil(matchNumber / 2)  + '"] .march-bracket__team:' + (isFirst ? 'first-of-type' : 'last-of-type')).attr('data-team', teamName).html(html).addClass('is-filled');
+            $('.march-round[data-region="' + region + '"][data-round="' + (round + 1) + '"] .march-bracket__matchup[data-match-number="' + Math.ceil(matchNumber / 2)  + '"] .march-bracket__team:' + (isFirst ? 'first-of-type' : 'last-of-type')).attr('data-team', teamName).html(html).addClass(classList);
         }
     },
 
@@ -92,7 +96,7 @@ module.exports =  {
             if (i === 1) {
                 return;
             }
-            $('.march-round[data-round="' + i + '"] .march-bracket__team[data-team="' + teamName + '"]').removeClass('is-winner is-filled').empty().removeAttr('data-team');
+            $('.march-round[data-round="' + i + '"] .march-bracket__team[data-team="' + teamName + '"]').removeClass('is-winner is-double is-filled').empty().removeAttr('data-team');
         }
 
         if (round > 1) {
